3b3340e086109f3907c5a9413434a4c4b1983296
gouder hicham <74071854+gouderhicham@users.noreply.github.com>
2022-11-22 19:40:44 +0100

    change sms service

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -1,9 +1,25 @@
-const apiKey = process.env.apiKey;
 export default function handler(req, res) {
-  let input = req.body.message;
-  let url = `https://api.callmebot.com/whatsapp.php?phone=+213776935088&text=${input}&apikey=${apiKey}`;
-  if (input === "") return;
-  fetch(url)
+  let text = req.body.message; 
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "User-Agent":
+        "Telegram Bot SDK - (https://github.com/irazasyed/telegram-bot-sdk)",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      text: text,
+      disable_web_page_preview: false,
+      disable_notification: false,
+      reply_to_message_id: 0,
+      chat_id: "5204325500",
+    }),
+  };
+    if (text === "") return;
+  fetch("https://api.telegram.org/bot5903144277%3AAAFnShua3yy8MhrNkaQfpQwvxW0JxjFxONQ/sendMessage", options)
+    .then((response) => response.json())
     .then((response) => res.json({ message: response }))
     .catch((err) => res.json({ message: err }));
+  
 }
